9efd22afce49327610255e14662f0393ae87b2a3
Владимир Шеламов <vovanostm@MacBook-Pro-Vladimir.local>
2016-03-08 12:18:27 +0300

    First add

---
+++ scripts/menu.js
@@ -0,0 +1,31 @@
+
+$(".m-n").click(function(event) {
+	// console.log(this.getAttribute('href'));
+	$('main').addClass('.vis');
+  var 
+    _href = this.getAttribute('href');
+    window.history.pushState(null,null,_href);
+	// console.log(this.getAttribute('href'));
+	$.ajax({ 
+  type: "POST", 
+  url: "work.php", 
+  data: "name=" + "get_page"+"&page="+_href,
+  dataType: "html", 
+  success: function(data){ 
+      event.preventDefault();
+  	  // $('main').animate({opacity: 0}, 1500, function() {});
+  	  $('main').empty();
+      $('main').append(data);
+
+  	  // $('main').animate({opacity: 1500},
+  	  // 	500, function() {});
+  	},
+  error: function(XMLHttpRequest, textStatus, errorThrown)
+  {
+      window.location.replace(_href);
+  }
+      
+     
+  });
+});
+  
