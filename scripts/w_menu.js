7712088079f5bb8e7106a0d138ce59d6350b649d
Владимир Шеламов <vovanostm@MacBook-Pro-Vladimir.local>
2016-03-11 00:27:53 +0300

    refr_styles

--- scripts/w_menu.js
+++ scripts/w_menu.js
@@ -8,6 +8,7 @@ function navigate(_href)
 {
   var up_hrf="";
   if (_href == "/") _href = "index.html";
+  alert(_href);
   for (var i=0; i < _href.length; i++)
   {
     if ((i == 0)&&(_href[i]!="/")) up_hrf+=_href[i];
