c8b68e29bde5a687d8c7d8b5394eeb9eef55da1b
Владимир Шеламов <vovanostm@87.250.245.73-red2.dhcp.yndx.net>
2016-03-20 18:58:58 +0300

    New_Inform

---
+++ minimaze.sh
@@ -0,0 +1,4 @@
+java -jar /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/yuicompressor-2.4.8.jar --type js -o /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/webshp/scripts/menu.js /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/webshp/scripts/w_menu.js
+java -jar /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/yuicompressor-2.4.8.jar --type js -o /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/webshp/scripts/font.js /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/webshp/scripts/w_font.js
+java -jar /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/yuicompressor-2.4.8.jar --type css -o /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/webshp/styles/style.css /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/webshp/styles/w_style.css
+java -jar /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/yuicompressor-2.4.8.jar --type css -o /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/webshp/styles/m_style.css /Users/vovanostm/Yandex.Disk.localized/МШП/HTML/webshp/styles/w_m_style.css
---
+++ mn_add.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./minimaze.sh
+./git-add
+git commit -m $1
+git push webshp master
