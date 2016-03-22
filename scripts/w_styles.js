d77e7354bb705527784dc7682afa48d3be00857b
Владимир Шеламов <vovanostm@MacBook-Pro-Vladimir.local>
2016-03-22 19:26:10 +0300

    local_fonts_styles

--- scripts/w_styles.js
+++ scripts/w_styles.js
@@ -1,26 +1,26 @@
 (function(){
-    function addFont() {
+    function addStyle() {
         var style = document.createElement('style');
         style.rel = 'stylesheet';
         document.head.appendChild(style);
-        style.textContent = localStorage.sourceExo2;
+        style.textContent = localStorage.sourceMainStyle;
     }
     try {
-        if (localStorage.sourceExo2) {
+        if (localStorage.sourceMainStyle) {
             // The font is in localStorage, we can load it directly
-            addFont();
+            addStyle();
         } else {
             // We have to first load the font file asynchronously
             var request = new XMLHttpRequest();
-            request.open('GET', '/fnts/exotwo/exotwo.css', true);
+            request.open('GET', '/styles/style.css', true);
 
             request.onload = function() {
                 if (request.status >= 200 && request.status < 400) {
                     // We save the file in localStorage
-                    localStorage.sourceExo2 = request.responseText;
+                    localStorage.sourceMainStyle = request.responseText;
 
                     // ... and load the font
-                    addFont();
+                    addStyle();
                 }
             }
 
