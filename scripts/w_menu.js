2642e276fe382e96d5447ca0f7ccba850756e912
Владимир Шеламов <vovanostm@MacBook-Pro-Vladimir.local>
2016-03-25 01:41:28 +0300

    new_nav

--- scripts/w_menu.js
+++ scripts/w_menu.js
@@ -1,23 +1,26 @@
 var
-  last_path=window.location.pathname;
-  scrll_old = 0,
-  scrll = 0;
-var stateObj = {
-  foo: "bar"
-};
-
-
+  last_path=window.location.pathname,
+  stateObj={"foo":"bar"},
+    menu_bottom = main_nav.offsetHeight,
+    menu_top = 0,
+    scrll_old = window.pageYOffset,
+    scrll = window.pageYOffset;
 
 function update_in_arefs() {
   var hrefs = document.getElementsByTagName("a");
   for (var i = 0; i < hrefs.length; i++) {
-    console.log(hrefs[i].href[0]);
     if (hrefs[i].getAttribute("href")[0] == "/") {
-      console.log(hrefs[i].innerHTML);
       if(!hrefs[i].classList.contains("no_aj"))
       hrefs[i].addEventListener("click", aref_click);
     }
   }
+}
+
+function aref_click(event) {
+  event.preventDefault();
+  hrf = this.getAttribute('href');
+  navigate(hrf);
+  if(hrf[0] != '#') window.history.pushState(stateObj, null, hrf);
 }
 
 function navigate(_href) {
@@ -40,13 +43,11 @@ function navigate(_href) {
     xhr.open('POST', '/work.php?', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.send(b_d); // 3. Отсылаем запрос
-    xhr.onreadystatechange = function() { // (3)
+    xhr.onreadystatechange = function() {
       if (xhr.readyState != 4) return;
       if (xhr.status != 200) {
-        window.location.replace(_href);
-        // alert(xhr.status + ': ' + xhr.statusText);
+        console.log("smbf_wrong");
       } else {
-        // document.getElementsByTagName("main")[0].innerHTML = '';
         document.getElementsByTagName("main")[0].innerHTML = xhr.responseText;
         update_in_arefs();
         last_path=window.location.pathname;
@@ -55,98 +56,31 @@ function navigate(_href) {
           _hash = _hash.substring(1);
           document.getElementById(_hash).scrollIntoView();
         } else {
-          document.body.scrollIntoView();
+          window.scrollTo(0,0);
         }
       }
     }
   }
 }
 
-
-function aref_click(event) {
-  event.preventDefault();
-  hrf = this.getAttribute('href');
-  navigate(hrf);
-  window.history.pushState(stateObj, null, hrf);
-}
-
 window.addEventListener("popstate", function(e) {
     if (last_path!=window.location.pathname)
+    {
       navigate(window.location.pathname+window.location.hash);
+    }
     last_path=window.location.pathname;
 }, false);
 
-update_in_arefs();
-
-// // 4. Если код ответа сервера не 200, то это ошибка
-// if (xhr.status != 200) {
-//   // обработать ошибку
-//   alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-// } else {
-//   // вывести результат
-//   alert( xhr.responseText ); // responseText -- текст ответа.
-// }
-
-// }
 function append_menu()
 {
   update_in_arefs();
-
-  var menu_bottom = main_nav.getBoundingClientRect().bottom + window.pageYOffset;
-
-  window.onscroll = function() {
-    scrll = window.pageYOffset;
-    if (scrll < scrll_old) {
-      console.log('up');
-      main_nav.setAttribute('style', "top:0px");
-
-    } else {
-      // if (window.pageYOffset > menu_bottom)
-      main_nav.setAttribute('style', "top:-" + menu_bottom + "px");
-
-      console.log(menu_bottom);
-      console.log('down');
-    }
-    scrll_old = scrll;
-    // window.pageYOffset ||
-    // document.getElementById('showScroll').innerHTML = scrolled + 'px';
-  }
+  window.onscroll = scroll;
 }
-
+function scroll(){
+    scrll = window.pageYOffset;
+    menu_top=(scrll < scrll_old)?(menu_top < 0)?menu_top+scrll_old-scrll:0:(menu_top > -menu_bottom)?menu_top+scrll_old-scrll:-menu_bottom;
+    menu_top=menu_top<-menu_bottom?-menu_bottom:menu_top>0?0:menu_top;
+    main_nav.setAttribute('style', "top:"+menu_top+"px");
+    scrll_old = scrll;
+}
 window.onload=append_menu;
-// document.addEventListener("DOMContentLoaded", append_menu);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $("a").click(function(event) {
-//   event.preventDefault();
-//   var
-//     _href = this.getAttribute('href');
-//   window.history.pushState(null,null,_href);
-// 	$.ajax({
-//   type: "POST",
-//   url: "work.php",
-//   data: "name=" + "get_page"+"&page="+_href,
-//   dataType: "html",
-//   success: function(data){
-//   	  $('main').empty();
-//       $('main').append(data);
-//   	},
-//   error: function(XMLHttpRequest, textStatus, errorThrown)
-//   {
-//       //window.location.replace(_href);
-//       console.log('error');
-//   }
-//   });
-// });
