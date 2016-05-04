0f4e96c9a83ca127bf28523fa77ccb2f7369bc5a
Vovanostm <axb48@yandex.ru>
2016-05-05 00:39:24 +0300

    fonts

--- scripts/w_font.js
+++ scripts/w_font.js
@@ -9,6 +9,13 @@
         if (localStorage.sourceExo2) {
             // The font is in localStorage, we can load it directly
             addFont();
+            var request = new XMLHttpRequest();
+            request.open('GET', '/fnts/exotwo/exotwo.css', true);
+
+            request.onload = function() {
+                if (request.status >= 200 && request.status < 400) {
+                    // We save the file in localStorage
+                    localStorage.sourceExo2 = request.responseText;
         } else {
             // We have to first load the font file asynchronously
             var request = new XMLHttpRequest();
